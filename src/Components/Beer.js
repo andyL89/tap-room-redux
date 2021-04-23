7bb8bbeb422644ae61c0b445dc3cde9b279d86d5
Andy Lawson <alawson89@gmail.com>
2021-04-23 16:35:29 -0700

    add color-coding for types on beer list

--- src/Components/Beer.js
+++ src/Components/Beer.js
@@ -2,6 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from 'styled-components';
 
+const Gose = styled.div`
+  color: rgb(255, 0, 213);
+  text-shadow: 1px 1px 2px #000000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 18px;
+`
+const Pale = styled.div`
+  color: rgb(255, 205, 0);
+  text-shadow: 1px 1px 2px #000000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 18px;
+`
+const Amber = styled.div`
+  color: rgb(255, 0, 0);
+  text-shadow: 1px 1px 2px #000000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 18px;
+`
+const Stout = styled.div`
+  color: rgb(144, 96, 0);
+  text-shadow: 1px 1px 2px #000000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 18px;
+`
+const IPA = styled.div`
+  color: rgb(255, 128, 0);
+  text-shadow: 1px 1px 2px #000000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 18px;
+`
+const Cider = styled.div`
+  color: rgb(26, 255, 0);
+  text-shadow: 1px 1px 2px #000000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 18px;
+`
 const StyledBeer = styled.div`
   cursor: pointer;
   margin-left: 25px;
@@ -9,15 +51,31 @@ const StyledBeer = styled.div`
   flex-direction: row;
   justify-content: space-around;
 `
-
 function Beer(props) {
   return (
     <>
     <StyledBeer onClick = {() => props.whenBeerClicked(props.id)}>
       <h3>{props.name}</h3>
       <h4>{props.brand}</h4>
-      <h4>{props.type}</h4>
-      <h5>${props.price}</h5>
+      {(props.type === "Gose" || props.type === "Sour") &&
+        <Gose>{props.type}</Gose>
+      }
+      {props.type === "Pale" &&
+        <Pale>{props.type}</Pale>
+      }
+      {props.type === "Amber" &&
+        <Amber>{props.type}</Amber>
+      }
+      {props.type === "Stout" &&
+        <Stout>{props.type}</Stout>
+      }
+      {props.type === "IPA" &&
+        <IPA>{props.type}</IPA>
+      }
+      {props.type === "Cider" &&
+        <Cider>{props.type}</Cider>
+      }
+      <h5>${props.price}/Pint</h5>
       <h5>{props.alcoholContent}% ABV</h5>
       <p>{props.pints}</p>
       </StyledBeer>
